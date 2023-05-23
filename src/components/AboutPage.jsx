e4e6fdb3aaeb53d2dd1eb54670a8cbd99f0781ff
gourav <gourav.radialcode@gmail.com>
2023-05-23 10:37:35 +0530

    our team section

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import GirlChatting from "../assets/images/png/GirlChatting.png";
 import MeetOurGirl from "../assets/images/png/MeetOurGirl.png";
@@ -8,6 +8,7 @@ import RakeshSheoran from "../assets/images/png/RakeshSheoran.png";
 import Linkedin from "../assets/images/svg/Linkedin.svg";
 
 function AboutPage() {
+  const [index, setIndex] = useState(1);
   return (
     <>
       <section className="AboutHeroSection pt-5">
@@ -15,9 +16,9 @@ function AboutPage() {
           <h2 className=" ff_Inter fw-bold fs_8xl text_primary text-center mb-0">
             About
           </h2>
-          <h1 className=" ff_Inter fw-bold fs_15xl text_primary text-center text-uppercase mt_n_20 opacity_1">
+          {/* <h1 className=" ff_Inter fw-bold fs_15xl text_primary text-center text-uppercase mt_n_20 opacity_1">
             HEXABIRDS
-          </h1>
+          </h1> */}
         </Container>
       </section>
       <section className="py-5   my-5 ">
@@ -76,22 +77,25 @@ function AboutPage() {
           <h2 className=" ff_Inter fw-bold fs_10xl text_dark text-center">
             Meet the team
           </h2>
-          <h2 className=" ff_Inter fw-bold fs_16xl text-center text-uppercase position-absolute text_dark start-50 translate-middle top-50 opacity_Low_1 index_n_1">
+          {/* <h2 className=" ff_Inter fw-bold fs_16xl text-center text-uppercase position-absolute text_dark start-50 translate-middle top-50 opacity_Low_1 index_n_1">
             MANAGEMENT
-          </h2>
+          </h2> */}
 
           <Row className=" justify-content-center">
-            <Col  xs={12} md={11} xl={8}>
+            <Col xs={12} md={11} xl={8}>
               {" "}
               <Row className=" justify-content-between pt-4 mt-3">
                 <Col xs={4} lg={3} className="text-center">
                   <img
+                    onClick={() => {
+                      setIndex(1);
+                    }}
                     className="w-100"
                     src={RakeshSheoran}
                     alt="RakeshSheoran"
                   />
-                  <div className=" position-relative index_1">
-                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary mb-0">
+                  <div className=" position-relative index_1 d-none d-sm-block ">
+                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
                       Rakesh Sheoran
                     </h2>
                     <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
@@ -108,12 +112,15 @@ function AboutPage() {
                 </Col>
                 <Col xs={4} lg={3} className="text-center">
                   <img
+                    onClick={() => {
+                      setIndex(2);
+                    }}
                     className="w-100"
                     src={JoginderSaini}
                     alt="JoginderSaini"
                   />
-                  <div className=" position-relative index_1">
-                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary mb-0">
+                  <div className=" position-relative index_1 d-none d-sm-block">
+                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
                       Joginder Saini
                     </h2>
                     <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
@@ -129,9 +136,16 @@ function AboutPage() {
                   </div>
                 </Col>{" "}
                 <Col xs={4} lg={3} className="text-center">
-                  <img className="w-100" src={Vikash} alt="Vikash" />
-                  <div className=" position-relative index_1">
-                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary mb-0">
+                  <img
+                    className="w-100"
+                    onClick={() => {
+                      setIndex(3);
+                    }}
+                    src={Vikash}
+                    alt="Vikash"
+                  />
+                  <div className=" position-relative index_1 d-none d-sm-block">
+                    <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
                       Vikash Boora
                     </h2>
                     <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
@@ -149,6 +163,48 @@ function AboutPage() {
               </Row>
             </Col>
           </Row>
+          <div className={index === 1 ? "d-block" : " d-none"}>
+            <div className=" position-relative index_1">
+              <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
+                Rakesh Sheoran
+              </h2>
+              <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
+                Website Developer
+              </h3>
+              <a href="#" className=" ff_Inter fw-normal fs_xsm text_primary">
+                <img className="pe-2" src={Linkedin} alt="Linkedin" />:
+                rakesh_123
+              </a>
+            </div>
+          </div>
+          <div className={index === 2 ? "d-block" : " d-none"}>
+            <div className=" position-relative index_1">
+              <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
+                Joginder Saini
+              </h2>
+              <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
+                App Developer & UI/UX
+              </h3>
+              <a href="#" className=" ff_Inter fw-normal fs_xsm text_primary">
+                <img className="pe-2" src={Linkedin} alt="Linkedin" />:
+                joginder_123
+              </a>
+            </div>
+          </div>
+          <div className={index === 3 ? "d-block" : " d-none"}>
+            <div className=" position-relative index_1">
+              <h2 className=" ff_Inter fw-bold fs_2xl text_primary">
+                Vikash Boora
+              </h2>
+              <h3 className=" ff_Inter fw-normal fs_sm text_primary mb-0">
+                Full Stack Developer
+              </h3>
+              <a href="#" className=" ff_Inter fw-normal fs_xsm text_primary">
+                <img className="pe-2" src={Linkedin} alt="Linkedin" />:
+                vikas_123
+              </a>
+            </div>
+          </div>
         </Container>
       </section>
     </>
